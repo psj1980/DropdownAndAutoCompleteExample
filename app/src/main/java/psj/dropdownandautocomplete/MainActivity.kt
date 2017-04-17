6dba62e2775ef3818cf64591d80bb12117f95b88
Pia Jensen <vi.har.det.godt@gmail.com>
2017-04-17 21:11:48 +0200

    layout updated

--- app/src/main/java/psj/dropdownandautocomplete/MainActivity.kt
+++ app/src/main/java/psj/dropdownandautocomplete/MainActivity.kt
@@ -27,38 +27,51 @@ class MainActivity : Activity() {
         // Set button on click event listener
         button.onClick { if (edit) makeViewsUneditable() else makeViewsEditable() }
 
-        // Listen for click on done button editText
-        editText.setOnEditorActionListener { v, actionId, event -> if(actionId == EditorInfo.IME_ACTION_DONE) { toastNumber(); true } else false }
-
-        // Listen for change of focus for editText
-        editText.setOnFocusChangeListener { v, hasFocus -> if(!hasFocus) toastNumber() }
 
 
-        // Autocomplete:
+        // NUMBER ONLY EditText:
+
+        // Listen for click on done button in EditText
+        editText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) { toastNumber(); true }
+            else false }
+
+        // Listen for change of focus away from EditText
+        editText.setOnFocusChangeListener { v, hasFocus -> if (!hasFocus) toastNumber() }
+
+
+
+        // AUTOCOMPLETE EditText:
+
+        // Create an arrayAdapter and set autocomplete's adapter to the new adapter
         val autoCompleteAdapter = ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, ingredients.toList())
         autoCompleteTextView.setAdapter(autoCompleteAdapter)
 
 
-        // android.R.layout.simple_spinner_item
-        // Spinner / Dropdown
-        val spinnerAdapter = ArrayAdapter(this, R.layout.spinner_item, courseType)
-        spinner.adapter = spinnerAdapter
-        spinner.setSelection(1)
-        // Herover kunne man have en getId(mainCourse: String) eller listen bør være en liste, hvor
-        // man kan finde det ønskede id(element) via find.
 
-        spinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+        // SPINNER (Dropdown):
+
+        // Create arrayAdapter with own spinner_item.xml
+        val spinnerAdapter = ArrayAdapter(this, R.layout.spinner_item, courseType)
+
+        // Set spinner's adapter to new adapter and set index 1 as default choice
+        spinner.adapter = spinnerAdapter
+        spinner.setSelection(1) // get fra DB, eller en listes index
+
+        // Listen for user selection in dropdown
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
 
             override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
                 toast(parent.getItemAtPosition(pos).toString())
             }
 
             override fun onNothingSelected(parent: AdapterView<out Adapter>?) {
-                spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                //spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             }
-        })
+        }
     }
 
+    // Find number in EditText and toast it
     fun toastNumber() {
 
         editText.setText(validateNumber(editText.text.toString()).toString())
@@ -70,18 +83,11 @@ class MainActivity : Activity() {
     fun makeViewsEditable() {
 
         edit = true
-
         autoCompleteTextView.isCursorVisible = true
         autoCompleteTextView.isClickable = true
         autoCompleteTextView.isFocusable = true
         autoCompleteTextView.isFocusableInTouchMode = true
-
         spinner.isEnabled = true
-
-        editText.isCursorVisible = true
-        editText.isClickable = true
-        editText.isFocusable = true
-        editText.isFocusableInTouchMode = true
     }
 
     // Set attributes to disable editing in an EditText view
@@ -92,21 +98,13 @@ class MainActivity : Activity() {
         autoCompleteTextView.isClickable = false
         autoCompleteTextView.isFocusable = false
         autoCompleteTextView.isFocusableInTouchMode = false
-
         spinner.isEnabled = false
-
-        editText.isCursorVisible = false
-        editText.isClickable = false
-        editText.isFocusable = false
-        editText.isFocusableInTouchMode = false
-
         hideKeyboard()
     }
 
-    fun hideKeyboard() {
+    // Hide the keyboard
+    fun hideKeyboard() { inputMethodManager.hideSoftInputFromWindow(exampleActivity.windowToken, 0) }
 
-        inputMethodManager.hideSoftInputFromWindow(exampleActivity.windowToken, 0)
-    }
-
+    // When the number-var is empty or has value < 1, 2 is returned, else return number
     fun validateNumber(number: String) = if (number.isEmpty() || number.toInt() < 1) 2 else number.toInt()
 }
